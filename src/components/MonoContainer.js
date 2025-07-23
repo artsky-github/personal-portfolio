37c61377bdbd36d6adb5515f6498db4f243c75c8
Arthur Levitsky <artsky.task@gmail.com>
2025-07-23 02:41:51 -0400

    removed old watercolor image and replaced with a new one for navbar background. Applied smooth transition hover effect for nav links.

--- src/components/MonoContainer.js
+++ src/components/MonoContainer.js
@@ -2,7 +2,7 @@ import styles from "@/styles/mono-container.module.css"
 
 export function MonoContainer({children, className = ""}) {
   return (<>
-    <div className = {`${styles.drawnBorder} ${styles.paperBackground} ${className} min-w-max`}>
+    <div className = {`${styles.drawnBorder} ${styles.paperBackground} ${className} shadow-lg min-w-max`}>
       {children}
     </div>
   </>)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { MonoContainer } from "./MonoContainer"
 export function Navbar() {
   return (<>
     <MonoContainer className = "flex justify-between">
-      <h1 className = {`${artskyFont.className} text-white text-5xl p-5`}>Arthur Levitsky</h1>
+      <h1 className = {`${artskyFont.className} text-white text-5xl p-5 pe-2`}>artsky</h1>
       <ul className = {`${artskyFont.className} flex gap-8 text-3xl align-middle items-center p-5 justify-end`}>
         <NavItem>Home</NavItem>
         <NavItem>Blog</NavItem>
@@ -20,6 +20,6 @@ export function Navbar() {
 function NavItem({children}) {
   return (
   <>
-    <li className = "hover:scale-115 hover:cursor-pointer">{children}</li>
+    <li className = {`${styles.navTransition} hover:scale-115 hover:cursor-pointer`}>{children}</li>
   </>)
 }
